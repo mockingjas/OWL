fc7b51af5541619b70c5770e25bd43b6e657fd53
Jasmine Mae Tolentino <jatolentino@factset.com>
2016-05-12 17:20:13 +0800

    Revert "Add Django init files"
    This reverts commit 0aa31cd680590cbf81d0ed64df0d35e514a7d553.

--- main.sql
+++ main.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*lib system main*/
 
 CREATE DATABASE owl_sys;
@@ -32,3 +33,35 @@ CREATE TABLE borrow(
 	FOREIGN KEY (username) REFERENCES user(username)
 );
 
+=======
+/*lib system main*/
+
+CREATE DATABASE owl_sys;
+
+CREATE TABLE book(
+	bookId INT(5) AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(100) NOT NULL,
+	isbn VARCHAR(100) NOT NULL,
+	genre VARCHAR(100) NOT NULL,
+	author VARCHAR(100) NOT NULL,
+	avail_flag BOOLEAN NOT NULL
+);
+
+CREATE TABLE user(
+	username VARCHAR(50) PRIMARY KEY,
+	/*adding some details, in case we have the notification :)*/
+	name VARCHAR(50) NOT NULL,
+	isAdmin BOOLEAN NOT NULL
+);
+
+CREATE TABLE borrow(
+	borrowId INT AUTO_INCREMENT PRIMARY KEY,
+	borrowDate DATE NOT NULL,
+	dueDate DATE NOT NULL,
+	returnDate DATE NOT NULL,
+	borrowReason VARCHAR(200),
+	bookId INT FOREIGN KEY REFERENCES books(bookId),
+	username VARCHAR(50) FOREIGN KEY REFERENCES user(username)
+);
+
+>>>>>>> parent of 0aa31cd... Add Django init files
