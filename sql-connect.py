da459c8e02dede2fe9d9071b03b34efe8433f4d0
mavilyn <gladys.mavilyn@gmail.com>
2016-05-12 21:41:24 +0800

    Init working sqlite-py connection

--- sql-connect.py
+++ sql-connect.py
@@ -1,2 +1,22 @@
 import sqlite3
-conn = splite3.connect(owl_sys.db)
+import json
+
+sqlite_file = 'owl_sys.db'
+conn = sqlite3.connect(sqlite_file)
+
+c = conn.cursor()
+
+c.execute('''SELECT username FROM user''')
+data = c.fetchall()
+
+for row in data:
+    print ("Let's talk about %s." % row)
+    
+c.execute('''SELECT bookId FROM book''')
+data = c.fetchall()
+
+for row in data:
+    print ("Let's talk about %s." % row)
+    
+json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
+print (json.dumps("\"foo\bar"))
